69f3c6692241c00fa0a279c0be9cad972e4dd2ef
eityamo <eityamo@gmail.com>
2022-05-02 00:25:04 +0900

    add フォーム画面の修正

--- app/forms/records_form.rb
+++ app/forms/records_form.rb
@@ -4,6 +4,11 @@ class RecordsForm
 
   attribute :myname, :string
   attribute :old, :integer
+  attribute :fromdate, :date
+  attribute :todate, :date
+  attribute :yourname, :string
+  attribute :getup, :integer
+  attribute :cleanup, :integer
+  attribute :note, :integer
 
-  validates :myname, :old, presence: true
 end
