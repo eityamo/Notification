46c36593dd3501925a9bc24dcf4e301146a35fe6
eityamo <eityamo@gmail.com>
2022-05-09 15:30:52 +0900

    'add:エラーメッセージの追加'

--- app/forms/records_form.rb
+++ app/forms/records_form.rb
@@ -4,12 +4,20 @@ class RecordsForm
   include ActiveModel::Validations
 
   attribute :myname, :string
-  attribute :old, :string
+  attribute :old, :integer
   attribute :fromdate, :date, default: -> { Time.now }
   attribute :todate, :date, default: -> { Time.now.since(1.day) }
   attribute :yourname, :string
   attribute :remark, :string
   attribute :getup, :string
   attribute :cleanup, :string
-  
+
+  validates :myname, { length: { maximum: 20 } }
+  validates :yourname, { length: { maximum: 20 } }
+  validates :remark, { length: { maximum: 50 } }
+  validate :start_end_check
+
+  def start_end_check
+    errors.add(:todate, "は開始日より前の日付は登録できません。") unless self.fromdate < self.todate
+  end
 end
