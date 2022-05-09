aca4d0686de6d4d69230b3e8b2f890133a96dc11
eityamo <eityamo@gmail.com>
2022-05-09 15:37:04 +0900

    'add:pdf化へのバリエーション'

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -5,11 +5,15 @@ class RecordsController < ApplicationController
 
   def create
     @record = RecordsForm.new(record_params)
+    if @record.valid?
     post_pdf = NotificationPdf::PostPdf.new(@record).render
       send_data post_pdf,
         filename: 'post_pdf.pdf',
         type: 'application/pdf',
         disposition: 'inline' #PDFをブラウザ上に出力　外すとダウンロード
+    else
+      render :new
+    end
   end
 
   private
