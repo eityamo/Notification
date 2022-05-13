28f6a6becd88ef4f3c59e9413068494732201c24
eityamo <eityamo@gmail.com>
2022-05-13 14:14:55 +0900

    訂正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def page_title(page_title = '')
+  def full_title(page_title = '')
     base_title = 'とどけ、反抗期'
 
     page_title.empty? ? base_title : page_title + " | " + base_title
