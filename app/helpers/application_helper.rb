896c92eda13c80c1f6f46fd3d17b2388b1607505
eityamo <eityamo@gmail.com>
2022-05-13 12:10:58 +0900

    twitterのOGP設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,13 @@ module ApplicationHelper
 
     page_title.empty? ? base_title : page_title + " | " + base_title
   end
+  
+  def full_url(path)
+    domain = if Rails.env.development?
+                     'http://0.0.0.0:3000'
+                   else
+                     'www.hankouki-todoke.com'
+                   end
+    "#{domain}#{path}"
+  end
 end
