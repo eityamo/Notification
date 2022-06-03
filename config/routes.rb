3ab2095f9010d08f307813e3975578b68a528fe4
eityamo <eityamo@gmail.com>
2022-06-03 17:43:16 +0900

    "add:ルーティングの設定"

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-	root 'records#new'
+	scope "(:locale)", locale: /#{I18n.available_locales.map(&:to_s).join("|")}/ do
+		root 'records#new'
 
-	get '/records', to: 'records#new'
-	get 'terms', to: 'records#terms'
-  get 'privacy_policy', to: 'records#privacy_policy'
-	resources :records, only: %i[create]
+		get '/records', to: 'records#new'
+		get 'terms', to: 'records#terms'
+	  get 'privacy_policy', to: 'records#privacy_policy'
+		resources :records, only: %i[create]
+	end
 end
