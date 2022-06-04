844ef1fd4fba32ee3304f7391fdf32584bb9bd24
eityamo <eityamo@gmail.com>
2022-06-04 09:35:07 +0900

    "add:言語のルーティング設定"

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
 	scope "(:locale)", locale: /#{I18n.available_locales.map(&:to_s).join("|")}/ do
 		root 'records#new'
-
 		get '/records', to: 'records#new'
 		get 'terms', to: 'records#terms'
 	  get 'privacy_policy', to: 'records#privacy_policy'
