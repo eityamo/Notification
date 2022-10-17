31ab0203d980da409cb34514a105325a9bcdcdb3
eityamo <eityamo@gmail.com>
2022-10-17 19:47:05 +0900

    add:ルーティング整理

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,11 @@ module Rename
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    # いらないルーティングを消去
+    initializer(:remove_action_mailbox_and_activestorage_routes, after: :add_routing_paths) { |app|
+      app.routes_reloader.paths.delete_if {|path| path =~ /activestorage/}
+      app.routes_reloader.paths.delete_if {|path| path =~ /actionmailbox/ }
+      }
 
     config.generators do |g|
       g.skip_routes true
